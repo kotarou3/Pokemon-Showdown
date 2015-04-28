30b051d1cc33abff7b273a3a015c98da7feb0bdb
Thiediev <chanjection@gmail.com>
2015-04-28 04:30:21 -0500

    added moves
    acid through blizzard

--- mods/rbyplus/moves.js
+++ mods/rbyplus/moves.js
@@ -3,5 +3,65 @@ exports.BattleMovedex = {
 		inherit: true,
 		basePower: 60,
 		type: "Water"
+	},
+	acid: {
+		inherit: true,
+		basePower: 95,
+		secondary: {
+			chance: 30,
+			boosts: {
+				def: -1
+			}
+		}
+	},
+	acidarmor: {
+		inherit: true,
+		pp: 20
+	},
+	agility: {
+		inherit: true,
+		pp: 20
+	},
+	aurorabeam: {
+		inherit: true,
+		basePower: 90,
+		secondary: {
+			chance: 30,
+			boosts: {
+				spd: -1,
+				spa: -1
+			}
+		}
+	},
+	barrage: {
+		inherit: true,
+		type: "Grass",
+		basePower: 200,
+		selfdestruct: true
+	},
+	barrier: {
+		inherit: true,
+		pp: 20
+	},
+	bide: {
+		inherit: true,
+		pp: 15
+	},
+	bind: {
+		inherit: true,
+		basePower: 15,
+		type: "Rock"
+	},
+	bite: {
+		inherit: true,
+		basePower: 80,
+		secondary: {
+			chance: 30,
+			volatilestatus: 'flinch'
+		}
+	},
+	blizzard: {
+		inherit: true,
+		accuracy: 80
 	}
 };
