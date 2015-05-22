b329161defa6645c6dc370b345c1652751ed69d5
Thiediev <chanjection@gmail.com>
2015-05-22 13:30:41 -0500

    changed some learnsets

--- mods/rbyplus/scripts.js
+++ mods/rbyplus/scripts.js
@@ -1,4 +1,10 @@
 exports.BattleScripts = {
 	inherit: 'gen1',
-	gen: 1
+	gen: 1,
+	init: function () {
+		this.modData('Learnsets', 'venusaur').learnset.takedown = [];
+		this.modData('Learnsets', 'charmander').learnset.cut = [];
+		this.modData('Learnsets', 'charmeleon').learnset.cut = [];
+		this.modData('Learnsets', 'charizard').learnset.cut = [];
+	}
 };
