2944a7a6d9b27357ad297105b9e9c5f9a6e0bde1
Thiediev <chanjection@gmail.com>
2015-05-29 14:11:27 -0500

    create pokedex.js

---
+++ mods/darkchocolate/pokedex.js
@@ -0,0 +1,36 @@
+exports.BattlePokedex = {
+	octillery: {
+		inherit: true,
+		types: ["Water","Fire"],
+		baseStats: {hp:75,atk:115,def:75,spa:115,spd:75,spe:45}
+	},
+	dusclops: {
+		inherit: true,
+		baseStats: {hp:10,atk:70,def:180,spa:60,spd:180,spe:25}
+	},
+	camerupt: {
+		inherit: true,
+		abilities: {0:"Water Absorb"}
+	},
+	cacturne: {
+		inherit: true,
+		baseStats: {hp:110,atk:115,def:60,spa:115,spd:60,spe:55},
+		abilities: {0:"Rough Skin"}
+	},
+	glaceon: {
+		inherit: true,
+		abilities: {DW:"Regenerator"}
+	},
+	empoleon: {
+		inherit: true,
+		abilities: {DW:"Snow Cloak"}
+	},
+	moltres: {
+		inherit: true,
+		abilities: {DW:"Regenerator"}
+	},
+	electivire: {
+		inherit: true,
+		types: ["Electric","Fighting"]
+	}
+};
