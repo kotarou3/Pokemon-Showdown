4b0972ecb1bb6911b3efb69f80eb87f906d9d716
Thiediev <chanjection@gmail.com>
2015-05-29 14:08:37 -0500

    create formats-data.js

---
+++ mods/darkchocolate/formats-data.js
@@ -0,0 +1,14 @@
+exports.BattleFormatsData = {
+	genesect: {
+		inherit: true,
+		tier: "OU"
+	},
+	excadrill: {
+		inherit: true,
+		tier: "OU"
+	},
+	deoxysspeed: {
+		inherit: true,
+		tier: "OU"
+	}
+};
