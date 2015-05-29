352d7df51a9c4212eb5195cf3de72327e995d6cf
Thiediev <chanjection@gmail.com>
2015-05-29 14:12:22 -0500

    create abilities.js

---
+++ mods/darkchocolate/abilities.js
@@ -0,0 +1,19 @@
+exports.BattleAbilities = {
+	"roughskin": {
+		inherit: true,
+		onAfterDamage: function (damage, target, source, move) {
+			if (source && source !== target && move && move.flags['contact']) {
+				this.damage(source.maxhp / 4, source, target);
+			}
+		}
+	},
+	"solidrock": {
+		inherit: true,
+		onSourceModifyDamage: function (damage, source, target, move) {
+			if (move.typeMod > 0) {
+				this.debug('Solid Rock neutralize');
+				return this.chainModify(0.50);
+			}
+		}
+	}
+};
