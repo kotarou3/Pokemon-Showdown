7508d665aff22202f50cc7f12a09b4d388aef814
Thiediev <chanjection@gmail.com>
2015-05-29 13:52:49 -0500

    Update moves.js

--- mods/darkchocolate/moves.js
+++ mods/darkchocolate/moves.js
@@ -8,9 +8,9 @@ exports.BattleMovedex = {
 				onSwitchIn: function (pokemon) {
 					var typeMod = this.getEffectiveness('Rock', pokemon);
 					var factor = 8;
-					if (typeMod == 1) factor = 6;
+					if (typeMod === 1) factor = 6;
 					if (typeMod >= 2) factor = 4;
-					if (typeMod == -1) factor = 16;
+					if (typeMod === -1) factor = 16;
 					if (typeMod <= -2) factor = 32;
 					var damage = this.damage(pokemon.maxhp/factor);
 				}
