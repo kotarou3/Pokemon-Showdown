71e0cbdc839bf3db3647a4bc0ff74103feafc0fe
PokerKid <kkaito1412.kk@gmail.com>
2015-07-28 21:24:31 +0800

    Update profile.js

--- chat-plugins/profile.js
+++ chat-plugins/profile.js
@@ -2,6 +2,6 @@ var commands = exports.commands = {
 
 	admine: function (target, room, user) {
 		if (!this.canBroadcast()) return;
-		ithis.sendReplyBox("<img src='http://leagueofillusions.weebly.com/uploads/2/7/3/8/27389937/393663066.png?479'; height=300 width=450><br><center><p><font color=red>I was the reason for your lost.</font></p></center>");
+		this.sendReplyBox("<img src='http://leagueofillusions.weebly.com/uploads/2/7/3/8/27389937/393663066.png?479'; height=300 width=450><br><center><p><font color=red>I was the reason for your lost.</font></p></center>");
 	}
 };
