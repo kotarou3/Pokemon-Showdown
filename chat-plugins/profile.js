db8267a359f63814d7f4abf41cc7a9dfe56ddad1
PokerKid <kkaito1412.kk@gmail.com>
2015-07-28 20:03:58 +0800

    Create profile.js

---
+++ chat-plugins/profile.js
@@ -0,0 +1,7 @@
+var commands = exports.commands = {
+
+    admine: function (target, room, user) {
+		if (!this.canBroadcast()) return;
+		this.sendReplyBox("<img src='http://leagueofillusions.weebly.com/uploads/2/7/3/8/27389937/393663066.png?479'; height=300 width=450><br><center><p><font color=red>I was the reason for your lost.</font></p></center>");
+	},
+}
