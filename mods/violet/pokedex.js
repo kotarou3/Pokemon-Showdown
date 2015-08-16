17be4196cca38aa95f266c7045eee7a441d148d1
Thiediev <chanjection@gmail.com>
2015-08-16 17:36:51 -0500

    create pokedex.js

---
+++ mods/violet/pokedex.js
@@ -0,0 +1,101 @@
+exports.BattlePokedex = {
+	charizard: {
+		inherit: true,
+		types: ["Fire","Dragon"]
+	},
+	butterfree: {
+		inherit: true,
+		types: ["Bug","Psychic"],
+		baseStats: {hp:70,atk:55,def:60,spa:95,spd:95,spe:80}
+	},
+	beedrill: {
+		inherit: true,
+		baseStats: {hp:65,atk:80,def:40,spa:45,spd:45,spe:130}
+	},
+	pidgeot: {
+		inherit: true,
+		baseStats: {hp:83,atk:110,def:75,spa:105,spd:105,spe:100}
+	},
+	raticate: {
+		inherit: true,
+		baseStats: {hp:65,atk:70,def:60,spa:65,spd:65,spe:115}
+	},
+	arbok: {
+		inherit: true,
+		baseStats: {hp:60,atk:105,def:69,spa:65,spd:65,spe:100}
+	},
+	nidoqueen: {
+		inherit: true,
+		baseStats: {hp:90,atk:92,def:87,spa:75,spd:75,spe:85}
+	},
+	ninetales: {
+		inherit: true,
+		types: ["Fire","Psychic"]
+	},
+	golbat: {
+		inherit: true,
+		baseStats: {hp:85,atk:90,def:80,spa:80,spd:80,spe:90}
+	},
+	vileplume: {
+		inherit: true,
+		baseStats: {hp:80,atk:105,def:85,spa:100,spd:100,spe:80}
+	},
+	parasect: {
+		inherit: true,
+		baseStats: {hp:90,atk:115,def:130,spa:80,spd:80,spe:30}
+	},
+	dugtrio: {
+		inherit: true,
+		baseStats: {hp:35,atk:110,def:50,spa:70,spd:70,spe:120}
+	},
+	golduck: {
+		inherit: true,
+		types: ["Water","Ghost"]
+	},
+	primeape: {
+		inherit: true,
+		baseStats: {hp:65,atk:105,def:60,spa:60,spd:60,spe:125}
+	},
+	poliwrath: {
+		inherit: true,
+		baseStats: {hp:90,atk:115,def:95,spa:70,spd:70,spe:70}
+	},
+	machamp: {
+		inherit: true,
+		baseStats: {hp:100,atk:130,def:100,spa:65,spd:65,spe:55}
+	},
+	magneton: {
+		inherit: true,
+		types: ["Electric","Rock"],
+		baseStats: {hp:120,atk:60,def:95,spa:120,spd:120,spe:70}
+	},
+	muk: {
+		inherit: true,
+		baseStats: {hp:145,atk:105,def:85,spa:75,spd:75,spe:50}
+	},
+	hypno: {
+		inherit: true,
+		baseStats: {hp:85,atk:103,def:80,spa:115,spd:115,spe:67},
+	},
+	electrode: {
+		inherit: true,
+		baseStats: {hp:65,atk:65,def:70,spa:110,spd:110,spe:140}
+	},
+	tangela: {
+		inherit: true,
+		baseStats: {hp:85,atk:55,def:115,spa:100,spd:100,spe:60}
+	},
+	electabuzz: {
+		inherit: true,
+		types: ["Electric","Fighting"]
+	},
+	magmar: {
+		inherit: true,
+		types: ["Fire","Fighting"],
+		baseStats: {hp:65,atk:95,def:57,spa:85,spd:85,spe:120}
+	},
+	flareon: {
+		inherit: true,
+		baseStats: {hp:115,atk:130,def:90,spa:110,spd:110,spe:65}
+	}
+};
