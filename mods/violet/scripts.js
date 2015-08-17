2b9064f6172f984a02b1ab24a6e4b58f4c5989cc
Thiediev <chanjection@gmail.com>
2015-08-16 20:28:23 -0500

    create scripts.js

---
+++ mods/violet/scripts.js
@@ -0,0 +1,437 @@
+exports.BattleScripts = {
+	inherit: 'gen1',
+	gen: 1,
+	// Battle scripts.
+	// runMove can be found in scripts.js. This function is the main one when running a move.
+	// It deals with the beforeMove and AfterMoveSelf events.
+	// This leads with partial trapping moves shennanigans after the move has been used.
+	// It also deals with how PP reduction works on gen 1.
+	runMove: function (move, pokemon, target, sourceEffect) {
+		move = this.getMove(move);
+		if (!target) target = this.resolveTarget(pokemon, move);
+		if (target.subFainted) delete target.subFainted;
+
+		this.setActiveMove(move, pokemon, target);
+
+		if (pokemon.movedThisTurn || !this.runEvent('BeforeMove', pokemon, target, move)) {
+			// Prevent invulnerability from persisting until the turn ends.
+			pokemon.removeVolatile('twoturnmove');
+			// Rampage moves end without causing confusion
+			delete pokemon.volatiles['lockedmove'];
+			this.clearActiveMove(true);
+			// This is only run for sleep.
+			this.runEvent('AfterMoveSelf', pokemon, target, move);
+			return;
+		}
+		if (move.beforeMoveCallback) {
+			if (move.beforeMoveCallback.call(this, pokemon, target, move)) {
+				this.clearActiveMove(true);
+				return;
+			}
+		}
+		pokemon.lastDamage = 0;
+		var lockedMove = this.runEvent('LockMove', pokemon);
+		if (lockedMove === true) lockedMove = false;
+		if (!lockedMove && (!pokemon.volatiles['partialtrappinglock'] || pokemon.volatiles['partialtrappinglock'].locked !== target)) {
+			pokemon.deductPP(move, null, target);
+			// On gen 1 moves are stored when they are chosen and a PP is deducted.
+			pokemon.side.lastMove = move.id;
+			pokemon.lastMove = move.id;
+		}
+		this.useMove(move, pokemon, target, sourceEffect);
+		this.singleEvent('AfterMove', move, null, pokemon, target, move);
+
+		// If rival fainted
+		if (target.hp <= 0) {
+			// We remove recharge
+			if (pokemon.volatiles['mustrecharge']) pokemon.removeVolatile('mustrecharge');
+			delete pokemon.volatiles['partialtrappinglock'];
+			// We remove screens
+			target.side.removeSideCondition('reflect');
+			target.side.removeSideCondition('lightscreen');
+			pokemon.removeVolatile('twoturnmove');
+		} else {
+			this.runEvent('AfterMoveSelf', pokemon, target, move);
+		}
+
+		// For partial trapping moves, we are saving the target
+		if (move.volatileStatus === 'partiallytrapped' && target && target.hp > 0) {
+			// Let's check if the lock exists
+			if (pokemon.volatiles['partialtrappinglock'] && target.volatiles['partiallytrapped']) {
+				// Here the partialtrappinglock volatile has been already applied
+				if (!pokemon.volatiles['partialtrappinglock'].locked) {
+					// If it's the first hit, we save the target
+					pokemon.volatiles['partialtrappinglock'].locked = target;
+				} else {
+					if (pokemon.volatiles['partialtrappinglock'].locked !== target && target !== pokemon) {
+						// The target switched, therefor, we must re-roll the duration, damage, and accuracy.
+						var duration = [2, 3][this.random(2)];
+						pokemon.volatiles['partialtrappinglock'].duration = duration;
+						pokemon.volatiles['partialtrappinglock'].locked = target;
+						// Duration reset thus partially trapped at 2 always.
+						target.volatiles['partiallytrapped'].duration = 2;
+						// We get the move position for the PP change.
+						var usedMovePos = -1;
+						for (var m in pokemon.moveset) {
+							if (pokemon.moveset[m].id === move.id) usedMovePos = m;
+						}
+						if (usedMovePos > -1 && pokemon.moveset[usedMovePos].pp === 0) {
+							// If we were on the middle of the 0 PP sequence, the PPs don't get reset in Violet.
+							pokemon.moveset[usedMovePos].pp = 0;
+							pokemon.isStale = 2;
+							pokemon.isStaleSource = 'ppoverflow';
+						}
+					}
+				}
+			} // If we move to here, the move failed and there's no partial trapping lock.
+		}
+	},
+	// tryMoveHit can be found on scripts.js
+	// This function attempts a move hit and returns the attempt result before the actual hit happens.
+	// It deals with partial trapping weirdness and accuracy bugs as well.
+	tryMoveHit: function (target, pokemon, move, spreadHit) {
+		var boostTable = [1, 4 / 3, 5 / 3, 2, 7 / 3, 8 / 3, 3];
+		var doSelfDestruct = true;
+		var damage = 0;
+
+		// First, check if the Pokémon is immune to this move.
+		if (move.ignoreImmunity !== true && !move.ignoreImmunity[move.type] && !target.runImmunity(move.type, true)) {
+			if (move.selfdestruct) {
+				this.faint(pokemon, pokemon, move);
+			}
+			return false;
+		}
+
+		// Now, let's calculate the accuracy.
+		var accuracy = move.accuracy;
+
+		// Partial trapping moves: true accuracy while it lasts
+		if (move.volatileStatus === 'partiallytrapped' && pokemon.volatiles['partialtrappinglock'] && target === pokemon.volatiles['partialtrappinglock'].locked) {
+			accuracy = true;
+		}
+
+		// If a sleep inducing move is used while the user is recharging, the accuracy is true.
+		if (move.status === 'slp' && target && target.volatiles['mustrecharge']) {
+			accuracy = true;
+		}
+
+		// OHKO moves only have a chance to hit if the user is at least as fast as the target
+		if (move.ohko) {
+			if (target.speed > pokemon.speed) {
+				this.add('-immune', target, '[ohko]');
+				return false;
+			}
+		}
+
+		// Calculate true accuracy for gen 1, which uses 0-255.
+		if (accuracy !== true) {
+			// Rage bug
+			if (move.id === 'rage' && pokemon.volatiles['ragemiss']) {
+				accuracy = 1;
+			} else {
+				accuracy = Math.floor(accuracy * 255 / 100);
+			}
+			// Check also for accuracy modifiers.
+			if (!move.ignoreAccuracy) {
+				if (pokemon.boosts.accuracy > 0) {
+					accuracy *= boostTable[pokemon.boosts.accuracy];
+				} else {
+					accuracy = Math.floor(accuracy / boostTable[-pokemon.boosts.accuracy]);
+				}
+			}
+			if (!move.ignoreEvasion) {
+				if (target.boosts.evasion > 0 && !move.ignorePositiveEvasion) {
+					accuracy = Math.floor(accuracy / boostTable[target.boosts.evasion]);
+				} else if (target.boosts.evasion < 0) {
+					accuracy *= boostTable[-target.boosts.evasion];
+				}
+			}
+		}
+		accuracy = this.runEvent('Accuracy', target, pokemon, move, accuracy);
+		// Moves that target the user do not suffer from the 1/256 miss chance.
+		if (move.target === 'self' && accuracy !== true) accuracy++;
+
+		// In RBY, there's a 1/256 chance of missing always, no matter what. Not in Violet.
+		if (accuracy !== true && this.random(256) > accuracy) {
+			this.attrLastMove('[miss]');
+			this.add('-miss', pokemon);
+			damage = false;
+		}
+
+		// If damage is 0 and not false it means it didn't miss, let's calc.
+		if (damage !== false) {
+			pokemon.lastDamage = 0;
+			if (move.multihit) {
+				var hits = move.multihit;
+				if (hits.length) {
+					// Yes, it's hardcoded... meh
+					if (hits[0] === 2 && hits[1] === 5) {
+						hits = [2, 2, 3, 3, 4, 5][this.random(6)];
+					} else {
+						hits = this.random(hits[0], hits[1] + 1);
+					}
+				}
+				hits = Math.floor(hits);
+				// In gen 1, all the hits have the same damage for multihits move
+				var moveDamage = 0;
+				var firstDamage;
+				var i;
+				for (i = 0; i < hits && target.hp && pokemon.hp; i++) {
+					if (i === 0) {
+						// First hit, we calculate
+						moveDamage = this.moveHit(target, pokemon, move);
+						firstDamage = moveDamage;
+					} else {
+						// We get the previous damage to make it fix damage
+						move.damage = firstDamage;
+						moveDamage = this.moveHit(target, pokemon, move);
+					}
+					if (moveDamage === false) break;
+					damage = (moveDamage || 0);
+					if (target.subFainted) {
+						i++;
+						break;
+					}
+				}
+				move.damage = null;
+				if (i === 0) return true;
+				this.add('-hitcount', target, i);
+			} else {
+				damage = this.moveHit(target, pokemon, move);
+			}
+		}
+
+		if (move.category !== 'Status') target.gotAttacked(move, damage, pokemon);
+
+		// Checking if substitute fainted
+		if (target.subFainted) doSelfDestruct = false;
+		if (move.selfdestruct && doSelfDestruct) {
+			this.faint(pokemon, pokemon, move);
+		}
+
+		// The move missed.
+		if (!damage && damage !== 0) {
+			// Delete the partial trap lock if necessary.
+			delete pokemon.volatiles['partialtrappinglock'];
+			return false;
+		}
+
+		if (move.ohko) this.add('-ohko');
+
+		if (!move.negateSecondary) {
+			this.singleEvent('AfterMoveSecondary', move, null, target, pokemon, move);
+			this.runEvent('AfterMoveSecondary', target, pokemon, move);
+		}
+
+		return damage;
+	},
+	// move Hit can be found on scripts.js
+	// It deals with the actual move hit, as the name indicates, dealing damage and/or effects.
+	// This function also deals with the Gen 1 Substitute behaviour on the hitting process.
+	moveHit: function (target, pokemon, move, moveData, isSecondary, isSelf) {
+		var damage = 0;
+		move = this.getMoveCopy(move);
+
+		if (!isSecondary && !isSelf) this.setActiveMove(move, pokemon, target);
+		var hitResult = true;
+		if (!moveData) moveData = move;
+
+		if (move.ignoreImmunity === undefined) {
+			move.ignoreImmunity = (move.category === 'Status');
+		}
+
+		// We get the sub to the target to see if it existed
+		var targetSub = (target) ? target.volatiles['substitute'] : false;
+		var targetHadSub = (targetSub !== null && targetSub !== false && (typeof targetSub !== 'undefined'));
+
+		if (target) {
+			hitResult = this.singleEvent('TryHit', moveData, {}, target, pokemon, move);
+
+			// Handle here the applying of partial trapping moves to Pokémon with Substitute
+			if (targetSub && moveData.volatileStatus && moveData.volatileStatus === 'partiallytrapped') {
+				target.addVolatile(moveData.volatileStatus, pokemon, move);
+			}
+
+			if (!hitResult) {
+				if (hitResult === false) this.add('-fail', target);
+				return false;
+			}
+
+			// Only run the hit events for the hit itself, not the secondary or self hits
+			if (!isSelf && !isSecondary) {
+				hitResult = this.runEvent('TryHit', target, pokemon, move);
+				if (!hitResult) {
+					if (hitResult === false) this.add('-fail', target);
+					// Special Substitute hit flag
+					if (hitResult !== 0) {
+						return false;
+					}
+				}
+				if (!this.runEvent('TryFieldHit', target, pokemon, move)) {
+					return false;
+				}
+			} else if (isSecondary && !moveData.self) {
+				hitResult = this.runEvent('TrySecondaryHit', target, pokemon, moveData);
+			}
+
+			if (hitResult === 0) {
+				target = null;
+			} else if (!hitResult) {
+				if (hitResult === false) this.add('-fail', target);
+				return false;
+			}
+		}
+
+		if (target) {
+			var didSomething = false;
+
+			damage = this.getDamage(pokemon, target, moveData);
+
+			// getDamage has several possible return values:
+			//
+			//   a number:
+			//     means that much damage is dealt (0 damage still counts as dealing
+			//     damage for the purposes of things like Static)
+			//   false:
+			//     gives error message: "But it failed!" and move ends
+			//   null:
+			//     the move ends, with no message (usually, a custom fail message
+			//     was already output by an event handler)
+			//   undefined:
+			//     means no damage is dealt and the move continues
+			//
+			// basically, these values have the same meanings as they do for event
+			// handlers.
+
+			if ((damage || damage === 0) && !target.fainted) {
+				if (move.noFaint && damage >= target.hp) {
+					damage = target.hp - 1;
+				}
+				damage = this.damage(damage, target, pokemon, move);
+				if (!(damage || damage === 0)) return false;
+				didSomething = true;
+			} else if (damage === false && typeof hitResult === 'undefined') {
+				this.add('-fail', target);
+			}
+			if (damage === false || damage === null) {
+				return false;
+			}
+			if (moveData.boosts && !target.fainted) {
+				this.boost(moveData.boosts, target, pokemon, move);
+
+				// Check the status of the Pokémon whose turn is not.
+				// When a move that affects stat levels is used, if the Pokémon whose turn it is not right now is paralyzed or
+				// burned, the correspoding stat penalties will be applied again to that Pokémon.
+				if (pokemon.side.foe.active[0] && pokemon.side.foe.active[0].status) {
+					// If it's paralysed, quarter its speed.
+					if (pokemon.side.foe.active[0].status === 'par') {
+						pokemon.side.foe.active[0].modifyStat('spe', 0.25);
+					}
+					// If it's burned, halve its attack.
+					if (pokemon.side.foe.active[0].status === 'brn') {
+						pokemon.side.foe.active[0].modifyStat('atk', 0.5);
+					}
+					pokemon.side.foe.active[0].update();
+				}
+			}
+			if (moveData.heal && !target.fainted) {
+				var d = target.heal(Math.floor(target.maxhp * moveData.heal[0] / moveData.heal[1]));
+				if (!d) {
+					this.add('-fail', target);
+					return false;
+				}
+				this.add('-heal', target, target.getHealth);
+				didSomething = true;
+			}
+			if (moveData.status) {
+				// Gen 1 bug: If the target has just used hyperbeam and must recharge, its status will be ignored and put to sleep.
+				// This does NOT revert the paralyse speed drop or the burn attack drop.
+				if (!target.status || moveData.status === 'slp' && target.volatiles['mustrecharge']) {
+					if (target.setStatus(moveData.status, pokemon, move)) {
+						// Gen 1 mechanics: The burn attack drop and the paralyse speed drop are applied here directly on stat modifiers.
+						if (moveData.status === 'brn') target.modifyStat('atk', 0.5);
+						if (moveData.status === 'par') target.modifyStat('spe', 0.25);
+					}
+				} else if (!isSecondary) {
+					if (target.status === moveData.status) {
+						this.add('-fail', target, target.status);
+					} else {
+						this.add('-fail', target);
+					}
+				}
+				didSomething = true;
+			}
+			if (moveData.forceStatus) {
+				if (target.setStatus(moveData.forceStatus, pokemon, move)) {
+					if (moveData.forceStatus === 'brn') target.modifyStat('atk', 0.5);
+					if (moveData.forceStatus === 'par') target.modifyStat('spe', 0.25);
+					didSomething = true;
+				}
+			}
+			if (moveData.volatileStatus) {
+				if (target.addVolatile(moveData.volatileStatus, pokemon, move)) {
+					didSomething = true;
+				}
+			}
+			if (moveData.sideCondition) {
+				if (target.side.addSideCondition(moveData.sideCondition, pokemon, move)) {
+					didSomething = true;
+				}
+			}
+			if (moveData.pseudoWeather) {
+				if (this.addPseudoWeather(moveData.pseudoWeather, pokemon, move)) {
+					didSomething = true;
+				}
+			}
+			// Hit events
+			hitResult = this.singleEvent('Hit', moveData, {}, target, pokemon, move);
+			if (!isSelf && !isSecondary) {
+				this.runEvent('Hit', target, pokemon, move);
+			}
+			if (!hitResult && !didSomething) {
+				if (hitResult === false) this.add('-fail', target);
+				return false;
+			}
+		}
+		var targetHasSub = false;
+		if (target) {
+			var targetSub = target.getVolatile('substitute');
+			if (targetSub !== null) {
+				targetHasSub = (targetSub.hp > 0);
+			}
+		}
+
+		// Here's where self effects are applied.
+		var doSelf = (targetHadSub && targetHasSub) || !targetHadSub;
+		if (moveData.self && (doSelf || moveData.self.volatileStatus === 'partialtrappinglock')) {
+			this.moveHit(pokemon, pokemon, move, moveData.self, isSecondary, true);
+		}
+
+		// Now we can save the partial trapping damage.
+		if (pokemon.volatiles['partialtrappinglock']) {
+			pokemon.volatiles['partialtrappinglock'].damage = pokemon.lastDamage;
+		}
+
+		// Apply move secondaries.
+		if (moveData.secondaries) {
+			for (var i = 0; i < moveData.secondaries.length; i++) {
+				// We check here whether to negate the probable secondary status if it's para, burn, or freeze.
+				// In the game, this is checked and if true, the random number generator is not called.
+				// That means that a move that does not share the type of the target can status it.
+				// If a move that was not fire-type would exist on Gen 1, it could burn a Pokémon.
+				if (!(moveData.secondaries[i].status && moveData.secondaries[i].status in {'brn':1, 'frz':1} && target && target.hasType(move.type))) {
+					var effectChance = Math.floor(moveData.secondaries[i].chance * 255 / 100);
+					if (typeof moveData.secondaries[i].chance === 'undefined' || this.random(256) < effectChance) {
+						this.moveHit(target, pokemon, move, moveData.secondaries[i], true, isSelf);
+					}
+				}
+			}
+		}
+		if (move.selfSwitch && pokemon.hp) {
+			pokemon.switchFlag = move.selfSwitch;
+		}
+
+		return damage;
+	}
+};
+	
